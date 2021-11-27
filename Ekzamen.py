43ea9cd0676f0f1fbcceae58902f3088b8a7a6b3
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-11-27 15:56:39 +0200

    Add files via upload

---
+++ Ekzamen.py
@@ -0,0 +1,96 @@
+            #№1
+#a=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#for i in a:
+#    if i < 5:
+#        print(i)
+            #№2
+#import random
+#a=1
+#n=int(input("="))
+#spisok=[random.randint(0,15) for i in range(n)]
+#for i in spisok:
+#    a*=i
+#print(a)
+#print(spisok)
+            #№3
+#numbers = [386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217]
+#for i in numbers:
+#    if i%2 == 0:
+#        print(i)
+#    elif i == 237:
+#        break
+            #№4
+#age=int(input(""))
+#if age<2:
+#    print("младенец")
+#elif 2 <= age > 4:
+#    print("малыш")
+#elif 4 <= age > 13:
+#    print("ребенок")
+#elif 13 <= age > 20:
+#    print("подросток")
+#elif 20 <= age > 65:
+#    print("взрослый")
+#elif 65 <= age:
+#    print("пожилой человек")
+            #№5
+#a=0
+#register=["z","x","c","v","b","n"]
+#g=[]
+#while register:
+#    del_user=register.pop(0)
+#    g.append(del_user)
+#for i in g:
+#    print(f"Пользователь верифицирован: {i}")
+            #№6
+#spisok=['pizza', 'falafel', 'carrot cake']
+#spisok_2=[]
+#spisok_2.append(spisok)
+#print(spisok_2)
+#banana=input("Мороженое=")
+#spisok_2.append(banana)
+#print(spisok_2)
+            #№7
+#a=0
+#lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+#for i in lst:
+#    if i<30 and i%3 == 0:
+#        print(i)
+#for i in lst:
+#    a+=i
+#print(a)
+            #№8
+#slovar={"a": 2, "b": 4, "c": 6, "d": 8}
+#for x,y in slovar.items():
+#    if y>2:
+#        print(y)
+            #№9
+
+            #№10
+
+            #№11
+#dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+#for x,y in dic.items():
+#    if y%2 == 0:
+#        print("even")
+#    else:
+#        print("odd")
+            #№12
+#import random
+#n=int(input("Введите количество рядков"))
+#m=int(input("Введите количество столбцов"))
+#a=[[random.randint(0,10) for j in range(m)] for i in range(n)]
+#print("-"*30)
+#for i in a:
+#    print(f"\t|{i}|")
+#c=[a[i][j]for i in range(n) for j in range(m) if i==j]
+#print(c)
+#v=[a[i][j]for i in range (int(input(""))) for j in range (int(input()))]
+#print(v)
+
+
+
+
+
+
+
